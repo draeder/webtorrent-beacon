933dda824327ef09f37a5802ab274d9cfa8ff034
draeder <draeder@gmail.com>
2021-07-31 18:28:51 -0500

    Updated to use torrent-discovery

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # webtorrent-beacon
- Get a beacon when a webtorrent containing your string gets a new peer
+Get a beacon when your webtorrent has a new peer
+
+##
+Creates a new webtorrent with your file name and data and sends a beacon whenever a new peer arrives on your torrent.
+
+## Install
+```js
+npm i webtorrent-beacon
+```
+
+## Usage
+```js
+let Beacon = require('webtorrent-beacon')
+
+let beacon = new Beacon('my filename.txt', 'my data', b => {
+ console.log('My torrent has a new peer:', b)
+})
+```
--- beacon.js
+++ beacon.js
@@ -1,50 +1,78 @@
 module.exports = Beacon
 
+const crypto = require('crypto')
 const WebTorrent = require('webtorrent')
+const Discovery = require('torrent-discovery')
+const addrToIPPort = require('addr-to-ip-port')
+const net = require('net')
+const Promise = require('bluebird')
 
-function Beacon(str, cb){
+let numPeers = 0
+let b = false
+
+function Beacon(name, data, cb){
+
+ let peerId = sha(Math.random().toString())
 
  let server = new WebTorrent()
 
- let buf = new Buffer.from(str)
- buf.name = str
+ let buf = new Buffer.from(data)
+ buf.name = name
  
  server.seed(buf, torrent => {
   // seeding
  })
- 
- let numPeers
- let b = false
 
- server.on('torrent', function (torrent) {
+ server.on('torrent', torrent => {
 
-  if(torrent.ready){
+  console.log(torrent.infoHash)
 
-   numPeers = torrent.numPeers
+  let opts = { infoHash: torrent.infoHash, peerId: peerId, port: 6881, dht: true }
 
-   torrent.on('wire', ()=> {
-    b = true
-    cb(b)
-   })
- 
-   let client = new WebTorrent()
- 
-   client.add(torrent.infoHash, torrent => {
- 
-    if(torrent.ready){
-     if(numPeers < torrent.numPeers){
-      b = true
-      cb(b)
-     }
-  
-     numPeers = torrent.numPeers
+  let discovery = new Discovery(opts)
 
-    }
-   })
-
-  }
+  let c = 0
+  discovery.on('peer', function (peer) {
+   c++
+   const peerAddress = { address: addrToIPPort(peer)[0], port: addrToIPPort(peer)[1] }
+   
+   if(c != 1){
+    checkConnection(peerAddress.address, peerAddress.port).then( ()=> {
+     console.log(peerAddress )
+     cb(true, c)
+    }, err => {
+     // connection was unsuccessful
+    })
+   }
+  })
  })
+}
 
- b = false
+function checkConnection(host, port, timeout) {
+ return new Promise( (resolve, reject) => {
+  timeout = timeout || 10000
+  
+  let timer = setTimeout( ()=> {
+   reject("timeout")
+   socket.destroy()
+  }, timeout)
+  
+  let socket = net.createConnection(port, host, ()=> {
+   clearTimeout(timer)
+   resolve()
+   socket.destroy()
+  })
+  
+  socket.on('error', err => {
+   clearTimeout(timer)
+   reject(err)
+  })
+ })
+}
 
+function sha(string){
+ let shasum = crypto.createHash('sha1')
+ shasum.update(string)
+ 
+ return shasum.digest('hex')
 }
